0ce1602785fc8f2f85f7350f046396120499d573
Peter Hunt <Peter92@users.noreply.github.com>
2019-05-13 12:29:41 +0100

    Add test for launching multiple standalone windows

---
+++ tests/standalone_instances.py
@@ -0,0 +1,27 @@
+"""This is a test to see if multiple window instances can be launched.
+By default, there can only be one QApplication, so this is an example of launching a second window using a new process.
+"""
+import sys
+import os
+sys.path.append(os.path.abspath(__file__).rsplit(os.path.sep, 2)[0])
+
+from vfxwindow import VFXWindow
+from vfxwindow.utils.Qt import QtWidgets
+
+
+class Window1(VFXWindow):
+    def __init__(self, *args, **kwargs):
+        super(Window1, self).__init__(*args, **kwargs)
+        self.setCentralWidget(QtWidgets.QPushButton('Open Window'))
+        self.centralWidget().clicked.connect(Window2.show)
+
+
+class Window2(VFXWindow):
+    def __init__(self, *args, **kwargs):
+        super(Window2, self).__init__(*args, **kwargs)
+        self.setCentralWidget(QtWidgets.QPushButton('Close'))
+        self.centralWidget().clicked.connect(self.close)
+
+
+if __name__ == '__main__':
+    Window1.show()
