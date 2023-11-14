37426c03fb975d57d2d19e0f5825e2ce69346a18
Peter Hunt <peter@huntfx.uk>
2023-11-14 16:09:31 +0000

    Fix build error

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 # Get the README.md text
-with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r', encoding='utf-8') as f:
     readme = f.read()
 
 # Parse vfxwindow/__init__.py for a version
