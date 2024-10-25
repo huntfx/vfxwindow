8bc2e738f612a69858bac21f38273ba7720f392a
Peter Hunt <peter@huntfx.uk>
2024-10-25 11:34:57 +0100

    Update Substance Designer callbacks to use new alias system

--- vfxwindow/substance_designer/callbacks.py
+++ vfxwindow/substance_designer/callbacks.py
@@ -1,99 +1,67 @@
 from __future__ import absolute_import
 
-from functools import partial
-
 import sd
 
-from ..abstract.callbacks import AbstractCallbacks, CallbackProxy
-
-
-APPLICATION = sd.getContext().getSDApplication()
-
-MANAGER = APPLICATION.getQtForPythonUIMgr()
+from ..abstract.callbacks import AbstractCallbacks
 
 
 class SubstanceDesignerCallbacks(AbstractCallbacks):
-    def add(self, name, func, *args, **kwargs):
-        """Register a callback.
+    """Substance Designer callbacks.
 
-        Callbacks:
-            file.load.before:
-                Called before when a file is loaded.
-                Signature: (filePath: str) -> None
+    Callbacks:
+        file.load.before:
+            Called before when a file is loaded.
+            Signature: (filePath: str) -> None
 
-            file.load.after:
-                Called after a file is loaded.
-                Signature: (filePath: str, succeed: bool, updated, bool) -> None
+        file.load.after:
+            Called after a file is loaded.
+            Signature: (filePath: str, succeed: bool, updated, bool) -> None
 
-            file.save.before:
-                Called before when a file is saved.
-                Signature: (filePath: str, parentPackagePath: str) -> None
+        file.save.before:
+            Called before when a file is saved.
+            Signature: (filePath: str, parentPackagePath: str) -> None
 
-            file.save.after:
-                Called after when a file is saved.
-                Signature: (filePath: str, succeed: bool) -> None
+        file.save.after:
+            Called after when a file is saved.
+            Signature: (filePath: str, succeed: bool) -> None
 
-            file.close.before:
-                Called before a file is closed.
-                Signature: (filePath: str) -> None
+        file.close.before:
+            Called before a file is closed.
+            Signature: (filePath: str) -> None
 
-            file.close.after:
-                Called after a file is saved.
-                Signature: (filePath: str, succeed: bool) -> None
+        file.close.after:
+            Called after a file is saved.
+            Signature: (filePath: str, succeed: bool) -> None
 
-            ui.graph.created:
-                Called when a new graphView is created.
-                Signature (graphViewID: int) -> None
+        ui.graph.created:
+            Called when a new graphView is created.
+            Signature (graphViewID: int) -> None
 
-            ui.explorer.created:
-                Called when a new explorer panel is created.
-                Signature (explorerID: int) -> None
+        ui.explorer.created:
+            Called when a new explorer panel is created.
+            Signature (explorerID: int) -> None
 
-            ui.explorer.selection.changed:
-                Called when the selection in the explorer panel changes.
-                Signature (explorerID: int) -> None
-        """
-        parts = name.split('.') + [None, None, None]
+        ui.explorer.selection.changed:
+            Called when the selection in the explorer panel changes.
+            Signature (explorerID: int) -> None
+    """
 
-        register = unregister = None
-        if parts[0] == 'file':
-            unregister = APPLICATION.unregisterCallback
-            if parts[1] == 'load':
-                if parts[2] == 'before':
-                    register = APPLICATION.registerBeforeFileLoadedCallback
-                elif parts[2] in ('after', None):
-                    register = APPLICATION.registerAfterFileLoadedCallback
-            elif parts[1] == 'save':
-                if parts[2] == 'before':
-                    register = APPLICATION.registerBeforeFileSavedCallback
-                elif parts[2] in ('after', None):
-                    register = APPLICATION.registerAfterFileSavedCallback
-            elif parts[1] == 'close':
-                if parts[2] == 'before':
-                    register = APPLICATION.registerAfterFileClosedCallback
-                elif parts[2] in ('after', None):
-                    register = APPLICATION.registerBeforeFileClosedCallback
+    def _setupAliases(self):
+        """Setup Substance Designer callback aliases."""
+        app = sd.getContext().getSDApplication()
+        manager = app .getQtForPythonUIMgr()
 
-        elif parts[0] == 'ui':
-            unregister = MANAGER.unregisterCallback
-            if parts[1] == 'graph':
-                if parts[2] == 'created':
-                    register = MANAGER.registerGraphViewCreatedCallback
-            elif parts[1] == 'explorer':
-                if parts[2] == 'created':
-                    register = MANAGER.registerExplorerCreatedCallback
-                elif parts[2] == 'selection':
-                    if parts[3] == 'changed':
-                        register = MANAGER.registerExplorerSelectionChangedCallback
+        self.aliases['file.load.before'] = (app.registerBeforeFileLoadedCallback, app.unregisterCallback)
+        self.aliases['file.load'] = self.aliases['file.load.after'] = (app.registerAfterFileLoadedCallback, app.unregisterCallback)
+        self.aliases['file.save.before'] = (app.registerBeforeFileSavedCallback, app.unregisterCallback)
+        self.aliases['file.save'] = self.aliases['file.save.after'] = (app.registerAfterFileSavedCallback, app.unregisterCallback)
+        self.aliases['file.close.before'] = (app.registerBeforeFileClosedCallback, app.unregisterCallback)
+        self.aliases['file.close'] = self.aliases['file.close.after'] = (app.registerAfterFileClosedCallback, app.unregisterCallback)
+        self.aliases['ui.graph.created'] = (manager.registerGraphViewCreatedCallback, manager.unregisterCallback)
+        self.aliases['ui.explorer.created'] = (manager.registerExplorerCreatedCallback, manager.unregisterCallback)
+        self.aliases['ui.explorer.selection.changed'] = (manager.registerExplorerSelectionChangedCallback, manager.unregisterCallback)
 
-        if register is None:
-            return super(SubstanceDesignerCallbacks, self).add(name, func, *args, **kwargs)
-
-        callback = CallbackProxy(name, register, unregister, func, args, kwargs)
-
-        # Only register if the Substance Designer window is loaded
-        if self.gui is not None and not self.gui._isHiddenSD:
-            callback.register()
-
-        self._callbacks.append(callback)
-        return callback
+    @property
+    def registerAvailable(self):
+        """Only register if the GUI is loaded."""
+        return self.gui is not None and not self.gui._isHiddenSD
